563f71c582be8592acfb2161b8e0df9fba5c7dff
SANA BEN <sana.bengannoune@gmail.com>
2021-05-28 11:36:28 +0000

    test increment /decrement number

---
+++ Calculator/Calculator/MainActivity.cs
@@ -0,0 +1,36 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Support.V7.App;
+using Android.Runtime;
+using Android.Widget;
+
+namespace Calculator
+{
+    [Activity(Label = "Calculator", Theme = "@style/AppTheme", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity
+    {
+        int i;
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.activity_main);
+
+            TextView nbr = FindViewById<TextView>(Resource.Id.nbr);
+
+            FindViewById<Button>(Resource.Id.Increment).Click += (o, e) =>
+            nbr.Text = (++i).ToString();
+
+            FindViewById<Button>(Resource.Id.Decrement).Click += (o, e) =>
+            nbr.Text = (--i).ToString();
+
+        }
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
+        {
+            Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+
+            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+    }
+}
